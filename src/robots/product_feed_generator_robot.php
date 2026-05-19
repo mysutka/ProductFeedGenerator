161ab3230260f446ec4c24bc92a9664f2bcc2ce4
Michal Vojan <michal.vojan@gmail.com>
2026-05-19 13:55:46 +0200

    Example robot updated, README.md updated

--- src/robots/product_feed_generator_robot.php
+++ src/robots/product_feed_generator_robot.php
@@ -14,9 +14,49 @@ class ProductFeedGeneratorRobot extends ApplicationRobot {
 
 		$todo_feeds = [];
 
-		$known_feeds = [
-			"heureka_cz", "heureka_sk", "zbozi_cz", "google_shopping", "google_merchants",
+		$feeds_config = [
+			# Create XML product feed for Heureka.cz price comparator
+			"heureka_cz" => [
+				"class" => \ProductFeedGenerator\Generator\HeurekaCzGenerator::class,
+				"output" => "product_feeds/heureka_cz.xml",
+			],
+			# Create XML product feed for Google Shopping price comparator
+			# As we want the feed to contain prices in EUR, we will use specific PriceFinder
+			"google_shopping" => [
+				"class" => \ProductFeedGenerator\Generator\GoogleShoppingGenerator::class,
+				"output" => "product_feeds/google_shopping.xml",
+				"options" => [
+					"price_finder" => $this->_getPriceFinder(["currency" => "EUR"]),
+				],
+			],
+			# Create CSV product feed for Google Merchants
+			# The output format is specified inside the GoogleMerchantsGenerator so it is not needed to put it as a parameter
+			"google_merchants" => [
+				"class" => \ProductFeedGenerator\Generator\GoogleMerchantsGenerator::class,
+				"output" => "product_feeds/google_merchants.csv",
+				"options" => [
+					"output_format" => "csv",
+				],
+			],
+			# Google Shopping feed with some more parameters
+			# - lang - slovak product translations will be used to create the feed
+			# - hostname - under some conditions might be useful to generate links to products with different hostname
+			# - eshop_url - url of the site
+			# - feed_title - short description of the eshop. some price comparators use it, some don't.
+			"google_shopping_sk" => [
+				"class" => \ProductFeedGenerator\Generator\GoogleShoppingGenerator::class,
+				"output" => "product_feeds/google_shopping_sk.xml",
+				"options" => [
+					"lang" => "sk",
+					"feed_title" => "ukážkový obchod",
+					"hostname" => "sk.gibona.com",
+					"eshop_url" => "ukazkovy-eshop.gibona.com",
+				],
+			],
 		];
+
+		$known_feeds = array_keys($feeds_config);
+
 		while($prm = array_shift($argv)) {
 			$todo_feeds[] = $prm;
 		}
@@ -35,49 +75,14 @@ class ProductFeedGeneratorRobot extends ApplicationRobot {
 			$todo_feeds = $known_feeds;
 		}
 
-		if (in_array("heureka_cz", $todo_feeds)) {
-		# Create XML product feed for Heureka.cz price comparator
-			$generator = new \ProductFeedGenerator\Generator\HeurekaCzGenerator($reader, [
-				"logger" => $this->logger,
-			]);
-			$generator->exportTo($ATK14_GLOBAL->getPublicRoot()."product_feeds/heureka_cz.xml");
+		foreach ($todo_feeds as $feed_name) {
+			if (!isset($feeds_config[$feed_name])) { continue; }
+			$config = $feeds_config[$feed_name];
+			$class = $config["class"];
+			$options = array_merge(["logger" => $this->logger], $config["options"] ?? []);
+			$generator = new $class($reader, $options);
+			$generator->exportTo($ATK14_GLOBAL->getPublicRoot().$config["output"]);
 		}
-
-		# Create XML product feed for Google Shopping price comparator
-		# As we want the feed to contain prices in EUR, we will use specific PriceFinder
-		if (in_array("google_shopping", $todo_feeds)) {
-			$generator = new \ProductFeedGenerator\Generator\GoogleShoppingGenerator($reader, [
-				"logger" => $this->logger,
-				"price_finder" => $this->_getPriceFinder(["currency" => "EUR"]),
-			]);
-			$generator->exportTo($ATK14_GLOBAL->getPublicRoot()."product_feeds/google_shopping.xml");
-		}
-
-		# Create CSV product feed for Google Merchants
-		# The output format is specified inside the GoogleMerchantsGenerator so it is not needed to put it as a parameter
-		if (in_array("google_merchants", $todo_feeds)) {
-			$generator = new \ProductFeedGenerator\Generator\GoogleMerchantsGenerator($reader, [
-				"logger" => $this->logger,
-				"output_format" => "csv",
-			]);
-			$generator->exportTo($ATK14_GLOBAL->getPublicRoot()."product_feeds/google_merchants.csv");
-		}
-
-		# Another example of product feed with some more parameters
-		# - lang - slovak product translations will be used to create the feed
-		# - hostname - under some conditions might be useful to generate links to products with different hostname
-		# - eshop_url - url of the site
-		# - feed_title - short description of the eshop. some price comparators use it, some don't.
-		$generator = new \ProductFeedGenerator\Generator\GoogleShoppingGenerator($reader, [
-			"logger" => $this->logger,
-			"lang" => "sk",
-			"feed_title" => "ukážkový obchod",
-			"hostname" => "ukazkovy-eshop.sk.gibona.com",
-			"eshop_url" => "ukazkovy-eshop.gibona.com",
-		]);
-		$generator->exportTo($ATK14_GLOBAL->getPublicRoot()."/product_feeds/google_shopping_sk.xml");
-
-		return;
 	}
 
 	function _getPriceFinder($options=[]) {
